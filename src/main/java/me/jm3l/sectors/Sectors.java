906b0648aeb267dc38eeff3a7f718dc715898f8e
Josh Melgar <josh.melgar@gmail.com>
2024-02-17 11:42:47 -0500

    Add base files

---
+++ src/main/java/me/jm3l/sectors/Sectors.java
@@ -0,0 +1,17 @@
+package me.jm3l.sectors;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Sectors extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
