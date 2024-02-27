9a0830e085ebac407f7cf42cb8e8103e5a2e1bc4
Josh Melgar <josh.melgar@gmail.com>
2024-02-27 11:38:37 -0500

    Misc cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,6 @@ plugins {
   id("xyz.jpenilla.run-paper") version "2.2.3"
 }
 
-group = "io.papermc.paperweight"
-version = "1.0.0-SNAPSHOT"
-description = "Test plugin for paperweight-userdev"
-
 java {
   // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
   toolchain.languageVersion.set(JavaLanguageVersion.of(17))
@@ -46,25 +42,17 @@ tasks {
   javadoc {
     options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
   }
-  processResources {
-    filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
-    val props = mapOf(
-      "name" to project.name,
-      "version" to project.version,
-      "description" to project.description,
-      "apiVersion" to "1.20"
-    )
-    inputs.properties(props)
-    filesMatching("plugin.yml") {
-      expand(props)
-    }
-  }
-
-  /*
-  reobfJar {
-    // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
-    // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
-    outputJar.set(layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar"))
-  }
-   */
+//  processResources {
+//    filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+//    val props = mapOf(
+//      "name" to project.name,
+//      "version" to project.version,
+//      "description" to project.description,
+//      "apiVersion" to "1.20"
+//    )
+//    inputs.properties(props)
+//    filesMatching("plugin.yml") {
+//      expand(props)
+//    }
+//  }
 }
